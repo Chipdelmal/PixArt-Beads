b979584cb0238b49e1bb806f2e0d26621e416c71
chipdelmal <chipdelmal@gmail.com>
2022-02-22 19:27:34 -0800

    Various automation improvements

--- colors.py
+++ colors.py
@@ -5,91 +5,200 @@
 #       https://www.designpieces.com/palette/
 ###############################################################################
 
-# https://lospec.com/palette-list/coldwood8
-COLD_WOOD = (
-    '#372e4d', '#5f699c', '#65aed6', '#a4ebcc',
-    '#effae6', '#f0b38d', '#b56d7f', '#614363'
-)
 
-# https://lospec.com/palette-list/nostalgia36
-NOSTALGIA = (
-    '#ff9e95', '#d97594', '#b34b96', '#852f85', '#5b226b', '#3d1757', 
-    '#3e4d66', '#62718b', '#8f9cb3', '#c1cada', '#d5ecea', '#63dbd5', 
-    '#51b7c7', '#428ab3', '#29608a', '#133862', '#1e2029', '#1f3741', 
-    '#2d5c56', '#387450', '#498f45', '#6fa341', '#a3c255', '#cad670', 
-    '#f0d661', '#e7a747', '#da7239', '#cd5632', '#993328', '#752626', 
-    '#472020', '#663329', '#8a4e34', '#ad7245', '#d1a568', '#f0d793'
-)
+COLD_WOOD = {
+    'name': 'CoolWood',
+    'source': 'https://lospec.com/palette-list/coldwood8',
+    'palette': (
+        '#372e4d', '#5f699c', '#65aed6', '#a4ebcc',
+        '#effae6', '#f0b38d', '#b56d7f', '#614363'
+    )
+}
 
-# https://www.designpieces.com/palette/game-boy-original-color-palette-hex-and-rgb/
-GB = (
-    '#cadc9f', '#9bbc0f', '#8bac0f', '#306230', '#0f380f'
-)
+NOSTALGIA = {
+    'name': 'Nostalgia',
+    'source': 'https://lospec.com/palette-list/nostalgia36',
+    'palette': (
+        '#ff9e95', '#d97594', '#b34b96', '#852f85', '#5b226b', '#3d1757', 
+        '#3e4d66', '#62718b', '#8f9cb3', '#c1cada', '#d5ecea', '#63dbd5', 
+        '#51b7c7', '#428ab3', '#29608a', '#133862', '#1e2029', '#1f3741', 
+        '#2d5c56', '#387450', '#498f45', '#6fa341', '#a3c255', '#cad670', 
+        '#f0d661', '#e7a747', '#da7239', '#cd5632', '#993328', '#752626', 
+        '#472020', '#663329', '#8a4e34', '#ad7245', '#d1a568', '#f0d793'
+    )
+}
 
+GB = {
+    'name': 'GB',
+    'source': 'https://www.designpieces.com/palette/game-boy-original-color-palette-hex-and-rgb/',
+    'palette': ('#cadc9f', '#9bbc0f', '#8bac0f', '#306230', '#0f380f')
+}
 
-# https://lospec.com/palette-list/endesga-32
-ENDESAGA = (
-    '#be4a2f', '#d77643', '#ead4aa', '#e4a672', '#b86f50', '#733e39', 
-    '#3e2731', '#a22633', '#e43b44', '#f77622', '#feae34', '#fee761', 
-    '#63c74d', '#3e8948', '#265c42', '#193c3e', '#124e89', '#0099db', 
-    '#2ce8f5', '#ffffff', '#c0cbdc', '#8b9bb4', '#5a6988', '#3a4466', 
-    '#262b44', '#181425', '#ff0044', '#68386c', '#b55088', '#f6757a', 
-    '#e8b796', '#c28569'
-)
+ENDESAGA = {
+    'name': 'Endesaga_32',
+    'source': 'https://lospec.com/palette-list/endesga-32',
+    'palette': (
+        '#be4a2f', '#d77643', '#ead4aa', '#e4a672', '#b86f50', '#733e39', 
+        '#3e2731', '#a22633', '#e43b44', '#f77622', '#feae34', '#fee761', 
+        '#63c74d', '#3e8948', '#265c42', '#193c3e', '#124e89', '#0099db', 
+        '#2ce8f5', '#ffffff', '#c0cbdc', '#8b9bb4', '#5a6988', '#3a4466', 
+        '#262b44', '#181425', '#ff0044', '#68386c', '#b55088', '#f6757a', 
+        '#e8b796', '#c28569'
+    )
+}
 
-# https://lospec.com/palette-list/nintendo-entertainment-system
-NES = (
-    '#000000', '#fcfcfc', '#f8f8f8', '#bcbcbc', '#7c7c7c', '#a4e4fc', 
-    '#3cbcfc', '#0078f8', '#0000fc', '#b8b8f8', '#6888fc', '#0058f8', 
-    '#0000bc', '#d8b8f8', '#9878f8', '#6844fc', '#4428bc', '#f8b8f8', 
-    '#f878f8', '#d800cc', '#940084', '#f8a4c0', '#f85898', '#e40058', 
-    '#a80020', '#f0d0b0', '#f87858', '#f83800', '#a81000', '#fce0a8', 
-    '#fca044', '#e45c10', '#881400', '#f8d878', '#f8b800', '#ac7c00', 
-    '#503000', '#d8f878', '#b8f818', '#00b800', '#007800', '#b8f8b8', 
-    '#58d854', '#00a800', '#006800', '#b8f8d8', '#58f898', '#00a844', 
-    '#005800', '#00fcfc', '#00e8d8', '#008888', '#004058', '#f8d8f8', 
-    '#787878'
-)
+NES = {
+    'name': 'NES', 
+    'source': 'https://lospec.com/palette-list/nintendo-entertainment-system', 
+    'palette': (
+        '#000000', '#fcfcfc', '#f8f8f8', '#bcbcbc', '#7c7c7c', '#a4e4fc', 
+        '#3cbcfc', '#0078f8', '#0000fc', '#b8b8f8', '#6888fc', '#0058f8', 
+        '#0000bc', '#d8b8f8', '#9878f8', '#6844fc', '#4428bc', '#f8b8f8', 
+        '#f878f8', '#d800cc', '#940084', '#f8a4c0', '#f85898', '#e40058', 
+        '#a80020', '#f0d0b0', '#f87858', '#f83800', '#a81000', '#fce0a8', 
+        '#fca044', '#e45c10', '#881400', '#f8d878', '#f8b800', '#ac7c00', 
+        '#503000', '#d8f878', '#b8f818', '#00b800', '#007800', '#b8f8b8', 
+        '#58d854', '#00a800', '#006800', '#b8f8d8', '#58f898', '#00a844', 
+        '#005800', '#00fcfc', '#00e8d8', '#008888', '#004058', '#f8d8f8', 
+        '#787878'
+    )
+}
+ 
+SLSO = {
+    'name': 'SLSO_8',
+    'source': 'https://lospec.com/palette-list/slso8',
+    'palette': (
+        '#0d2b45', '#203c56', '#544e68', '#8d697a', '#d08159', '#ffaa5e', 
+        '#ffd4a3', '#ffecd6'
+    )
+}
 
-# https://lospec.com/palette-list/slso8
-SLSO = (
-    '#0d2b45', '#203c56', '#544e68', '#8d697a', '#d08159', '#ffaa5e', 
-    '#ffd4a3', '#ffecd6'
-)
+MASTEROS = {
+    'name': 'MASTEROS',
+    'source': 'https://lospec.com/palette-list/le-masteros-paletas',
+    'palette': (
+        '#210c5c', '#760760', '#bd0463', '#ff2951', '#ff8f1b', '#ffce2f', 
+        '#fdfdfd', '#fed347', '#f5a20a', '#e66d19', '#d32b2c', '#9e093d', 
+        '#571845', '#601423', '#902b0e', '#b94b09', '#ce7923', '#e2a53b', 
+        '#f7d053', '#f8f8f8', '#fdca84', '#f6965a', '#e75f32', '#b73f2c', 
+        '#881f27', '#590023', '#005638', '#32723f', '#618c46', '#90a64d', 
+        '#b6c363', '#dae17e', '#ffff99', '#96ea6f', '#0ace37', '#00a732', 
+        '#008131', '#005d30', '#003b2f', '#062a78', '#104d95', '#1a70b3', 
+        '#2495d1', '#2ebcf1', '#72e0ff', '#d1ffff', '#a0d6ee', '#70addd', 
+        '#4185cc', '#165cb8', '#1b358c', '#401d6c', '#69358b', '#944eab', 
+        '#c168cd', '#ee84ee', '#f7bbef', '#ffedef', '#ffb9c9', '#ff7f9e', 
+        '#f93166', '#ba226b', '#7b1270', '#340075', '#4a0020', '#6f2737', 
+        '#934d4e', '#b77365', '#dd9a7c', '#ffc498', '#fffacd', '#e4db99', 
+        '#c8bc67', '#a7a041', '#72883e', '#3c703b', '#0b0b0b', '#2f2f2f', 
+        '#535353', '#7b7b7b', '#a5a5a5', '#d1d1d1', '#ffffff'
+    )
+}
 
-# https://lospec.com/palette-list/le-masteros-paletas
-MASTEROS = (
-    '#210c5c', '#760760', '#bd0463', '#ff2951', '#ff8f1b', '#ffce2f', 
-    '#fdfdfd', '#fed347', '#f5a20a', '#e66d19', '#d32b2c', '#9e093d', 
-    '#571845', '#601423', '#902b0e', '#b94b09', '#ce7923', '#e2a53b', 
-    '#f7d053', '#f8f8f8', '#fdca84', '#f6965a', '#e75f32', '#b73f2c', 
-    '#881f27', '#590023', '#005638', '#32723f', '#618c46', '#90a64d', 
-    '#b6c363', '#dae17e', '#ffff99', '#96ea6f', '#0ace37', '#00a732', 
-    '#008131', '#005d30', '#003b2f', '#062a78', '#104d95', '#1a70b3', 
-    '#2495d1', '#2ebcf1', '#72e0ff', '#d1ffff', '#a0d6ee', '#70addd', 
-    '#4185cc', '#165cb8', '#1b358c', '#401d6c', '#69358b', '#944eab', 
-    '#c168cd', '#ee84ee', '#f7bbef', '#ffedef', '#ffb9c9', '#ff7f9e', 
-    '#f93166', '#ba226b', '#7b1270', '#340075', '#4a0020', '#6f2737', 
-    '#934d4e', '#b77365', '#dd9a7c', '#ffc498', '#fffacd', '#e4db99', 
-    '#c8bc67', '#a7a041', '#72883e', '#3c703b', '#0b0b0b', '#2f2f2f', 
-    '#535353', '#7b7b7b', '#a5a5a5', '#d1d1d1', '#ffffff'
-)
+SGB_A = {
+    'name': 'SGB_M1A',
+    'source': 'https://lospec.com/palette-list/super-game-boy-menu-1-palette-a',    
+    'palette': ('#ffe8cf', '#df904f', '#af2850', '#301850')
+}
 
-# https://lospec.com/palette-list/super-game-boy-menu-1-palette-a
-SGB_A = ('#ffe8cf', '#df904f', '#af2850', '#301850')
+SGB_B = {
+    'name': 'SGB_M1B',
+    'source': 'https://lospec.com/palette-list/super-game-boy-menu-1-palette-b',    
+    'palette': ('#dfd8c0', '#cfb070', '#b05010', '#000000')
+}
 
-# https://lospec.com/palette-list/super-game-boy-menu-1-palette-b
-SGB_B = ('#dfd8c0', '#cfb070', '#b05010', '#000000')
+SGB_C = {
+    'name': 'SGB_M1C',
+    'source': 'https://lospec.com/palette-list/super-game-boy-menu-1-palette-c',    
+    'palette': ('#ffc0ff', '#ef9850', '#9f3860', '#3f389f')
+}
 
-# https://lospec.com/palette-list/super-game-boy-menu-1-palette-c
-SGB_C = ('#ffc0ff', '#ef9850', '#9f3860', '#3f389f')
+SGB_D = {
+    'name': 'SGB_M1D',
+    'source': 'https://lospec.com/palette-list/super-game-boy-menu-1-palette-d',    
+    'palette': ('#fff8af', '#c0804f', '#ff0000', '#501800')
+}
 
-# https://lospec.com/palette-list/super-game-boy-menu-1-palette-d
-SGB_D = ('#fff8af', '#c0804f', '#ff0000', '#501800')
+SUPER_ST = {
+    'name': 'Super_16',
+    'source': 'https://lospec.com/palette-list/super16',
+    'palette': (
+        '#902068', '#f81868', '#ffa880', '#ff7000', '#a80010', '#ffa800', 
+        '#ffffa8', '#a8e038', '#5800a8', '#6828ff', '#ffffff', '#e0d0ff', 
+        '#a070c8', '#683090', '#481868', '#000000'
+    )
+}
 
-# https://lospec.com/palette-list/super16
-SUPER_ST = (
-    '#902068', '#f81868', '#ffa880', '#ff7000', '#a80010', '#ffa800', 
-    '#ffffa8', '#a8e038', '#5800a8', '#6828ff', '#ffffff', '#e0d0ff', 
-    '#a070c8', '#683090', '#481868', '#000000'
-)
+MF_SXT = {
+    'name': 'MF_16',
+    'source': 'https://lospec.com/palette-list/mf-16',
+    'palette': (
+        '#160b27', '#3e4768', '#8e98b9', '#dee6fe', '#f3c41c', '#f98617', 
+        '#d42c4f', '#5f0267', '#4a10e8', '#2197f0', '#52daf7', '#bb4d11', 
+        '#ed7fb8', '#30fca6', '#17ba7d', '#056244'
+    )
+}
+
+ARTKAL_48 = {
+    'name': 'ARTKAL_48',
+    'source': 'https://artkalfusebeads.com/collections/mini-c-2-6mm-artkal-beads/products/cc48-artkal-mini-beads-48-colors-box-set-diy-beads',
+    'palette': (
+        '#111312', '#8A8A95', '#C1C1C3', '#DBD8D8', '#E0E6E2', '#DDDDDB',
+        '#A3302F', '#7F5946', '#997054', '#D8C7A2', '#E7DFC5', '#E8E2C6', 
+        '#E01D25', '#F2422A', '#F280B1', '#FAB9E7', '#EFBBA5', '#E1BAB8', 
+        '#DD277D', '#331971', '#491E8C', '#A46EC8', '#CFAAE7', '#C9B8E5', 
+        '#6DB5EF', '#62B2F8', '#77A6F3', '#3EA8F8', '#4B83E2', '#1C2097', 
+        '#C2DFE1', '#CFE5DE', '#AFE4EA', '#44CED2', '#4B9EBF', '#42ACCE', 
+        '#C0ECB7', '#7DD969', '#3CB982', '#1D9B39', '#137452', '#0B1413', 
+        '#ECE49B', '#F1E766', '#FADE3D', '#FEB30A', '#FEBB6D', '#F98A53', 
+    )
+}
+
+PEAR_36 = {
+    'name': 'Pear_36',
+    'source': 'https://lospec.com/palette-list/pear36',
+    'palette': (
+        '#5e315b', '#8c3f5d', '#ba6156', '#f2a65e', '#ffe478', '#cfff70', 
+        '#8fde5d', '#3ca370', '#3d6e70', '#323e4f', '#322947', '#473b78', 
+        '#4b5bab', '#4da6ff', '#66ffe3', '#ffffeb', '#c2c2d1', '#7e7e8f', 
+        '#606070', '#43434f', '#272736', '#3e2347', '#57294b', '#964253', 
+        '#e36956', '#ffb570', '#ff9166', '#eb564b', '#b0305c', '#73275c', 
+        '#422445', '#5a265e'
+    )
+}
+
+PIXLTEN = {
+    'name': 'Pixlten',
+    'source': 'https://lospec.com/palette-list/pixlten',
+    'palette': (
+        '#ffffff', '#dfeef0', '#b2cdd4', '#597585', '#22313b', '#18191a', 
+        '#f6ffb2', '#c9f562', '#a0d43f', '#2f8a21', '#126126', '#0a4023', 
+        '#072913', '#fffdc4', '#ffff63', '#f5e03d', '#f5c13d', '#e08320', 
+        '#a14115', '#7d2614', '#ffbfc3', '#ff7a7c', '#f53d4d', '#c21146', 
+        '#8f0b42', '#570b3a', '#310536', '#faadff', '#e96bff', '#b246f0', 
+        '#7325cc', '#311891', '#0b0b57', '#7fceff', '#59a1ff', '#3b89e3', 
+        '#2d5dbd', '#1e2d8f', '#121a5c', '#151515', '#000000'
+    )
+}
+
+ZANT = {
+    'name': 'Zant',
+    'source': 'https://lospec.com/palette-list/zant',
+    'palette': (
+        '#97c5e4', '#1a1d96', '#494cdd', '#617ce8', '#7299e8', '#8cb0e2', 
+        '#b9dbdf', '#d4ece7', '#bd5378', '#ce6737', '#e18856', '#f5c573', 
+        '#f0eac9', '#ffffff', '#d1f0a6', '#89df43', '#4fd032', '#1ab720', 
+        '#0da14d', '#08815d', '#544c78', '#824ba1', '#ce61a8', '#df7cdb', 
+        '#d8adec', '#ddd0ee', '#7fb7b7', '#73979f', '#617181', '#545b6c'
+
+    )
+}
+
+SWEETIE_16 = {
+    'name': 'Sweetie_16',
+    'source': 'https://lospec.com/palette-list/sweetie-16',
+    'palette': (
+        '#1a1c2c', '#5d275d', '#b13e53', '#ef7d57', '#ffcd75', '#a7f070', 
+        '#38b764', '#257179', '#29366f', '#3b5dc9', '#41a6f6', '#73eff7', 
+        '#f4f4f4', '#94b0c2', '#566c86', '#333c57'
+    )
+}
--- main.py
+++ main.py
@@ -7,16 +7,30 @@ import auxiliary as aux
 
 
 # (fld, nme) = ('./img', 'APC.png')
-(fld, nme) = ('/home/chipdelmal/Documents/PixelatorBeads', 'mechYellow.png')
-(downscale, upscale) = ((32, 32), 20)
-(colorsNumber, method) = (12, 1)
-colorPalette = col.SGB_A
+(fld, nme) = ('/home/chipdelmal/Documents/PixelatorBeads', 'Lynn.png')
+(downscale, upscale) = (48, 15)
+(colorsNumber, method) = (8, 1)
+pals = (
+    col.COLD_WOOD, col.NOSTALGIA, col.GB, col.ENDESAGA, col.NES,
+    col.SLSO, col.MASTEROS, col.SGB_A, col.SGB_B, col.SGB_C, col.SGB_D,
+    col.SUPER_ST, col.MF_SXT, col.ARTKAL_48, col.PEAR_36, col.PIXLTEN,
+    col.ZANT, col.SWEETIE_16
+)
 
+colDict = pals[0]
+
+(paletteName, colorPalette) = (colDict['name'], colDict['palette'])
 ###############################################################################
 # Load Image
 ###############################################################################
 pth = path.join(fld, nme)
 img = Image.open(pth).convert('RGB')
+###############################################################################
+# Check Scale
+###############################################################################
+if type(downscale) is not tuple:
+    (wo, ho) = img.size
+    downscale = (downscale, int((ho/wo)*downscale))
 ###############################################################################
 # Quantize
 ###############################################################################
@@ -36,11 +50,12 @@ img = img.resize(upscaleSize, Image.NEAREST)
 ###############################################################################
 # Save Scaled Image
 ###############################################################################
-pthUG = path.join(fld, aux.pthUG+nme)
+pth = '{}-{}-{}'.format(paletteName, str(downscale[0]), nme)
+pthUG = path.join(fld, aux.pthUG+pth)
 img.save(pthUG)
 ###############################################################################
 # Add grid onto the image
 ###############################################################################
 img = imread(pthUG)
-img = aux.gridOverlay(img, upscale)
-imwrite(path.join(fld, aux.pthGD+nme), img)
+img = aux.gridOverlay(img, upscale, gridColor=(0, 0, 0))
+imwrite(path.join(fld, aux.pthGD+pth), img)
