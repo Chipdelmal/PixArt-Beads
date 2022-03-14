d6c9a6b61bebd4fcdd6b01b42ee147c272406e85
chipdelmal <chipdelmal@gmail.com>
2022-03-14 16:50:32 -0700

    Swatches update

--- generateSwatches.py
+++ generateSwatches.py
@@ -1,7 +1,7 @@
 
 from os import path
 from glob import glob
-import auxiliary as aux
+import functions as aux
 
 PTH = './palettes/'
 palettes = sorted(glob(path.join(PTH, '*.plt')))
@@ -12,12 +12,13 @@ tRow = "<tr><td>{}</td><td><img src='./{}.png'></td><td><a href={}>{}</a></td></
 ###############################################################################
 for palette in palettes:
     pal = aux.readPaletteFile(palette)
-    img = aux.genColorSwatch(pal, 500, 20)
+    img = aux.genColorSwatch(pal['palette'], 500, 20)
     img.save(path.join(PTH, pal['name']+'.png'))
 ###############################################################################
 # Text for README
 ###############################################################################
 (head, body, tail) = ("<table><tr><th>Code</th><th>Palette</th><th>Source</th></tr>", "", "</table>")
+palette = palettes[0]
 for palette in palettes:
     pal = aux.readPaletteFile(palette)
     body = body+tRow.format(
---
+++ palettes/FrostyGB_4.plt
@@ -0,0 +1,7 @@
+FrostyGB_4
+https://lospec.com/palette-list/frosty-gb
+#000000
+#3b8ae9
+#98c0f3
+#ffffff
+
